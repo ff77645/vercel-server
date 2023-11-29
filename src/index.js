0861c1da52b7907889861cf4650a81d7bc7f206c
fy <fy77645@126.com>
2023-11-29 18:00:19 +0800

    调试

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import { Hono } from 'hono'
-import { query } from '@ifyour/deeplx'
 import { serve } from '@hono/node-server'
+import { sql } from "@vercel/postgres"
+import dotenv from 'dotenv'
+
+if(process.env.NODE_ENV === 'development'){
+  dotenv.config({
+    path:`.env.development.local`
+  })
+}
 
 const app = new Hono()
 
 app
-  .get('/', (c) => c.redirect('/translate'))
-  .get('/translate', (c) => c.text('Please use POST method :)'))
-  .post('/translate', async (c) => {
-    const params = await c.req.json().catch(() => ({}))
-    const result = await query(params)
-    return c.json(result, result.code)
+  .get('/', (c) => {
+    return c.text('Hello world!')
+  })
+  .get('/data',async (c)=>{
+    const { rows } = await sql`SELECT * from COMPANY`;
+    return c.json(rows)
   })
 
-serve(app)
+serve({
+  fetch:app.fetch,
+  port:5000
+})
